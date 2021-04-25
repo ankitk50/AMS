ecc2955c3a2547a1f1545b3c04ccefd9f31e8294
ankit <ankitk50@gmail.com>
2021-04-26 01:29:52 +0200

    task1.1 complete

--- systemstate.py
+++ systemstate.py
@@ -1,3 +1,5 @@
+from simparam import SimParam
+
 class SystemState(object):
     
     """
@@ -22,7 +24,14 @@ class SystemState(object):
         :return: system_state object
         """
         # TODO Task 1.1.1: Your code goes here
-        pass
+        self. sim= SimParam()
+        self.sys_state=SystemState(self)
+
+        self.server_busy=False
+        self.buffer_content=0
+       
+
+
 
     def add_packet_to_server(self):
         """
@@ -30,7 +39,13 @@ class SystemState(object):
         :return: True if server is not busy and packet has been added successfully.
         """
         # TODO Task 1.1.2: Your code goes here
-        pass
+        # when sever is free and there is nothing in the buffer, packet is served directly
+        if not self.server_busy and self.buffer_content==0:
+            self.server_busy=True
+           # self.buffer_content-=1 #FIXME 
+            return True
+        else:
+            return False
 
     def add_packet_to_queue(self):
         """
@@ -38,15 +53,22 @@ class SystemState(object):
         :return: True if buffer/queue is not full and packet has been added successfully.
         """
         # TODO Task 1.1.2: Your code goes here
-        pass
+        #when sever is busy and buffer content is not full
+        if self.buffer_content<self.sim.S  and self.server_busy: #FIXME
+            self.buffer_content+=1
+            return True
+      
 
     def complete_service(self):
         """
         Reset server status to idle after a service completion.
         """
         # TODO Task 1.1.3: Your code goes here
+        self.server_busy=False
+
+
         # TODO Task 2.4.3: Your code goes here somewhere
-        pass
+        
 
     def start_service(self):
         """
@@ -54,4 +76,7 @@ class SystemState(object):
         :return: True if buffer is not empty and a stored packet is being served.
         """
         # TODO Task 1.1.3: Your code goes here
-        pass
+        if self.buffer_content>0 and self.server_busy:
+            return True
+        else:
+            return False
